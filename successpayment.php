0fd49c75bf1aa96837b19952ac481cd43be95898
Brychan Artanto <brychan58@gmail.com>
2022-11-29 09:17:10 +0700

    Change to .php

--- successpayment.php
+++ successpayment.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="successpayment.css">
+    <link rel="stylesheet" href="css/successpayment.css">
 </head>
 <body>
     <div id="success">
