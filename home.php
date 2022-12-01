e3d1f5f1f5ce9a6803ce51a967e54731ce95f5f8
vincent-kartamulya <vincent.learn01@gmail.com>
2022-12-01 14:25:52 +0700

    Vincent ganti lagi

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
     <title>Beranda</title>
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-    <link rel="stylesheet" href="css/homer.css">
+    <link rel="stylesheet" href="css/homeu.css">
 </head>
 <body>
     <?php
