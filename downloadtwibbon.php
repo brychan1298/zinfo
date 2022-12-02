c73671a9ddffe0ad0d295a94a4a6cc36be2c4066
vincent-kartamulya <vincent.learn01@gmail.com>
2022-12-02 12:56:09 +0700

    twibbon design

---
+++ downloadtwibbon.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/downloadtwibbon.css">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav id="nav">
+        <div>
+            <ul id="ul">
+                <li>
+                    <a href="Beranda.html">
+                        <img src="Asset/Logo Zinfo 2.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="Tentang.html">Home</a>
+                </li>
+                <li>
+                    <a href="Materi.html">About Us</a>
+                </li>
+                <li>
+                    <a href="Mengajar.html">Event</a>
+                </li>
+                <li>
+                    <a href="Daftar.html">Twibbon</a>
+                </li>
+            </ul>
+        </div>
+        
+
+        <!-- RESPONSIVE MENU -->
+        <div id="toggle">open</div>
+        <div id="menu" class="menu-responsive">
+            <i class="fas fa-bars menu"></i>
+        </div>
+        
+        <div>
+            <ul id="ul2">
+                <li id="login">
+                    <button>Log In</button>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="Asset/cart.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="Asset/profil.png" alt="">
+                    </a>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Twibbon -->
+    <div id = "twibbon">
+        <div id = "twibbon-section">
+            <img src="Asset/logo-1.png" alt="">
+            <div class="twibbon-section-logo-p">
+                <p> Bank Central Asia </p>
+            </div>
+            <div class="twibbon-desc">
+                <div class = twibbon-desc-item>
+                    <img src="Asset/time-icon.png">
+                    <p> a year ago </p>
+                </div>
+                <div class = twibbon-desc-item>
+                    <img src="Asset/user-icon.png">
+                    <p> 60M user </p>
+                </div>
+            </div>
+            <div class="twibbon-section-name">
+                    <p> PPBP/PPTI BCA 2024</p>
+            </div>
+            <div class="twibbon-section-template">
+                <img src="Asset/download-twibbon.png">
+            </div>
+            <div class="twibbon-section-button">
+                <button> DOWNLOAD </button>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <div id="footer-wrapper">
+        <div id="footer">
+            <div class="footer-box1 a">
+                <img src="Asset/Logo ZInfo 2.png" alt="">
+            </div>
+            <div class="footer-box2 a">
+                <h2>ZInfo</h2>
+    
+                <p>About Us</p>
+                <p>Event</p>
+                <p>Twibbon</p>
+            </div>
+            <div class="footer-box3 a">
+                <h2>Copyright</h2>
+    
+                <p>Privacy Policy</p>
+                <p>Terms & Condition</p>
+            </div>
+            <div class="footer-box4 a">
+                <h2>Follow Us</h2>
+    
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-ig.png" alt="">
+                    </div>
+                    <div>
+                        Instagram
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-fb.png" alt="">
+                    </div>
+                    <div>
+                        Facebook
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-twitter.png" alt="">
+                    </div>
+                    <div>
+                        Twitter
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-tiktok.png" alt="">
+                    </div>
+                    <div>
+                        Tik Tok
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-youtube.png" alt="">
+                    </div>
+                    <div>
+                        Youtube
+                    </div>
+                </div>
+            </div>
+            <div class="footer-box5 a">
+                <h2>Contact Us</h2>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-wa.png" alt="">
+                    </div>
+                    <div>
+                        (+1) 415 1234567
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-email.png" alt="">
+                    </div>
+                    <div>
+                        info@zinfo.com
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h4>© 2022 ZInfo. All rights reserved.</h4>
+    </div>
+</body>
+</html>
---
+++ twibbon.php
@@ -0,0 +1,493 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/twibbon.css">
+</head>
+<body>
+    <!-- NAVBAR -->
+    <nav id="nav">
+        <div>
+            <ul id="ul">
+                <li>
+                    <a href="Beranda.html">
+                        <img src="Asset/Logo Zinfo 2.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="Tentang.html">Home</a>
+                </li>
+                <li>
+                    <a href="Materi.html">About Us</a>
+                </li>
+                <li>
+                    <a href="Mengajar.html">Event</a>
+                </li>
+                <li>
+                    <a href="Daftar.html">Twibbon</a>
+                </li>
+            </ul>
+        </div>
+        
+
+        <!-- RESPONSIVE MENU -->
+        <div id="toggle">open</div>
+        <div id="menu" class="menu-responsive">
+            <i class="fas fa-bars menu"></i>
+        </div>
+        
+        <div>
+            <ul id="ul2">
+                <li id="login">
+                    <button>Log In</button>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="Asset/cart.png" alt="">
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <img src="Asset/profil.png" alt="">
+                    </a>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
+
+    <!-- BANNER -->
+    <div id="banner">
+        <div>
+            <img src="Asset/banner-event-2.png" alt="" class="banner-img">
+        </div>
+        <div class="caroul-circle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+
+     <!-- EVENT -->
+     <div id="twibbon">
+        <div class="twibbon-upper">
+            Your Activity
+        </div>
+        
+        <div id="twibbon-section">
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-1.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>PPTI/PPBP BCA</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-1.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                Bank Central Asia
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                a year ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                60M user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-2.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>BINUSIAN 2026</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-2.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                Universitas Bina Nusantara
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                3 month ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                60k user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-3.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>HARI BUKU NASIONAL</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-3.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                Kemendikbud
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                5 month ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                30M user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-4.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>EDU COMPETITION</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-4.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                Genbi Jawara
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                2 year ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                10M user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-5.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>PRESIDENSI G20</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-5.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                Humas KAI
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                8 month ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                124k user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-6.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>HARI ANGKLUNG NASIONAL</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-5.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                MEDIABRITA
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                2 days ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                10k user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-7.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>HARI BUKU NASIONAL</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-7.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                PAPAN TULIS OFFICIAL
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                29 days ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                50k user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="twibbon-list">
+                <div class="twibbon-list-img">
+                    <img src="Asset/twibbon-8.png" alt="">
+                </div>
+                <div class="twibbon-list-desc">
+                    <h2>JENIUS SCIENCE STUDENT OLYMPIAD</h2>
+                    <div class="twibbon-list-logo">
+                        <div class="twibbon-list-logo-icon">
+                            <img src="Asset/logo-8.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                PT. Jenius Science Indonesia
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-time">
+                        <div class="twibbon-list-time-icon">
+                            <img src="Asset/time-icon.png" alt="">
+                        </div>
+                        <div>
+                            <p>
+                                7 days ago
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="twibbon-list-user">
+                        <div class="twibbon-list-user-icon">
+                            <img src="Asset/user-icon.png" alt="">
+                        </div>
+                        <div class="twibbon-list-user-count">
+                            <p>
+                                9k user
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <div id="footer-wrapper">
+        <div id="footer">
+            <div class="footer-box1 a">
+                <img src="Asset/Logo ZInfo 2.png" alt="">
+            </div>
+            <div class="footer-box2 a">
+                <h2>ZInfo</h2>
+    
+                <p>About Us</p>
+                <p>Event</p>
+                <p>Twibbon</p>
+            </div>
+            <div class="footer-box3 a">
+                <h2>Copyright</h2>
+    
+                <p>Privacy Policy</p>
+                <p>Terms & Condition</p>
+            </div>
+            <div class="footer-box4 a">
+                <h2>Follow Us</h2>
+    
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-ig.png" alt="">
+                    </div>
+                    <div>
+                        Instagram
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-fb.png" alt="">
+                    </div>
+                    <div>
+                        Facebook
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-twitter.png" alt="">
+                    </div>
+                    <div>
+                        Twitter
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-tiktok.png" alt="">
+                    </div>
+                    <div>
+                        Tik Tok
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-youtube.png" alt="">
+                    </div>
+                    <div>
+                        Youtube
+                    </div>
+                </div>
+            </div>
+            <div class="footer-box5 a">
+                <h2>Contact Us</h2>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-wa.png" alt="">
+                    </div>
+                    <div>
+                        (+1) 415 1234567
+                    </div>
+                </div>
+                <div class="footer-follow-us">
+                    <div>
+                        <img src="Asset/icon-email.png" alt="">
+                    </div>
+                    <div>
+                        info@zinfo.com
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h4>© 2022 ZInfo. All rights reserved.</h4>
+    </div>
+</body>
+</html>
