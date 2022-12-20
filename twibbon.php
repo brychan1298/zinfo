83c897d7e845fd7396587c29667cbde282200d8d
vincent-kartamulya <vincent.learn01@gmail.com>
2022-12-20 23:53:26 +0700

    twibbon page 1

--- twibbon.php
+++ twibbon.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="css/twibbon.css">
 </head>
 <body>
+    <?php
+        include "DBconn.php";
+        $twibbons = mysqli_query($conn, "SELECT * FROM event WHERE `logo` IS NOT NULL AND `twibbon` IS NOT NULL") or die(mysqli_error($conn));
+    ?>
     <!-- NAVBAR -->
     <?php include "navbar.php" ?>
 
@@ -25,333 +29,58 @@
 
 
      <!-- EVENT -->
-     <div id="twibbon">
+    <div id="twibbon">
         <div class="twibbon-upper">
             Your Activity
         </div>
         
         <div id="twibbon-section">
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-1.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>PPTI/PPBP BCA</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-1.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                Bank Central Asia
-                            </p>
-                        </div>
+            <?php
+                foreach($twibbons as $twibbon):
+            ?>
+                <div class="twibbon-list">
+                    <div class="twibbon-list-img">
+                        <img src="Asset/<?= $twibbon["Twibbon"] ?>" alt="">
                     </div>
+                    <div class="twibbon-list-desc">
+                        <h2><?= $twibbon["Nama"] ?></h2>
+                        <div class="twibbon-list-logo">
+                            <div class="twibbon-list-logo-icon">
+                                <img src="Asset/<?= $twibbon["logo"] ?>" alt="">
+                            </div>
+                            <div>
+                                <p>
+                                    <?= $twibbon["NamaOrganizer"] ?>
+                                </p>
+                            </div>
+                        </div>
 
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
+                        <div class="twibbon-list-time">
+                            <div class="twibbon-list-time-icon">
+                                <img src="Asset/time-icon.png" alt="">
+                            </div>
+                            <div>
+                                <p>
+                                    a year ago
+                                </p>
+                            </div>
                         </div>
-                        <div>
-                            <p>
-                                a year ago
-                            </p>
-                        </div>
-                    </div>
 
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                60M user
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-2.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>BINUSIAN 2026</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-2.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                Universitas Bina Nusantara
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                3 month ago
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                60k user
-                            </p>
+                        <div class="twibbon-list-user">
+                            <div class="twibbon-list-user-icon">
+                                <img src="Asset/user-icon.png" alt="">
+                            </div>
+                            <div class="twibbon-list-user-count">
+                                <p>
+                                    60M user
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-3.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>HARI BUKU NASIONAL</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-3.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                Kemendikbud
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                5 month ago
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                30M user
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-4.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>EDU COMPETITION</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-4.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                Genbi Jawara
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                2 year ago
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                10M user
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-5.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>PRESIDENSI G20</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-5.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                Humas KAI
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                8 month ago
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                124k user
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-6.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>HARI ANGKLUNG NASIONAL</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-5.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                MEDIABRITA
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                2 days ago
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                10k user
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-7.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>HARI BUKU NASIONAL</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-7.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                PAPAN TULIS OFFICIAL
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                29 days ago
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                50k user
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="twibbon-list">
-                <div class="twibbon-list-img">
-                    <img src="Asset/twibbon-8.png" alt="">
-                </div>
-                <div class="twibbon-list-desc">
-                    <h2>JENIUS SCIENCE STUDENT OLYMPIAD</h2>
-                    <div class="twibbon-list-logo">
-                        <div class="twibbon-list-logo-icon">
-                            <img src="Asset/logo-8.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                PT. Jenius Science Indonesia
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-time">
-                        <div class="twibbon-list-time-icon">
-                            <img src="Asset/time-icon.png" alt="">
-                        </div>
-                        <div>
-                            <p>
-                                7 days ago
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="twibbon-list-user">
-                        <div class="twibbon-list-user-icon">
-                            <img src="Asset/user-icon.png" alt="">
-                        </div>
-                        <div class="twibbon-list-user-count">
-                            <p>
-                                9k user
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
+            <?php 
+                endforeach
+            ?>
         </div>
     </div>
 
